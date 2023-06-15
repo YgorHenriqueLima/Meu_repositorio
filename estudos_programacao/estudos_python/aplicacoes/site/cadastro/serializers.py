c2ad32fc3bc1639aa8fb1b32f24abe9809f2ad58
ygorzkodes <ygorhenriquelima106@gmail.com>
2023-06-15 15:30:20 -0300

    adicionando alguns arquivos

--- estudos_programacao/estudos_python/aplicacoes/site/cadastro/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Aluno
-
-class AlunoSerialize(serializers.ModelSerializer):
-    class Meta:
-        model = Aluno
-        fields = ('id', 'name')
-        
